0f509ed61ad29bdf1d8e13d614026ae02a35af12
ddd <985890193@qq.com>
2023-01-12 15:05:32 +0800

    redis LocalDateTime 序列化与反序列化

--- spring-boot-redis/src/main/java/com/example/springbootredis/common/config/RedisConfig.java
+++ spring-boot-redis/src/main/java/com/example/springbootredis/common/config/RedisConfig.java
@@ -15,28 +15,27 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory) {
         // 方案一
-//        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-//        redisTemplate.setConnectionFactory(connectionFactory);
-//        return redisTemplate;
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
 
         // 方案二
-        RedisTemplate<String, Serializable> template = new RedisTemplate<>();
-        // 配置连接工厂
-        template.setConnectionFactory(connectionFactory);
-        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
-        Jackson2JsonRedisSerializer<Object> jacksonSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        jacksonSerializer.setObjectMapper(ObjectMapperConfig.objectMapper);
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        // 使用StringRedisSerializer来序列化和反序列化redis的key,value采用json序列化
-        template.setKeySerializer(stringRedisSerializer);
-        template.setValueSerializer(jacksonSerializer);
-        // 设置hash key 和value序列化模式
-        template.setHashKeySerializer(stringRedisSerializer);
-        template.setHashValueSerializer(jacksonSerializer);
-        template.afterPropertiesSet();
-
-        return template;
+//        RedisTemplate<String, Serializable> template = new RedisTemplate<>();
+//        // 配置连接工厂
+//        template.setConnectionFactory(connectionFactory);
+//        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+//        Jackson2JsonRedisSerializer<Object> jacksonSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+//        jacksonSerializer.setObjectMapper(ObjectMapperConfig.objectMapper);
+//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+//        // 使用StringRedisSerializer来序列化和反序列化redis的key,value采用json序列化
+//        template.setKeySerializer(stringRedisSerializer);
+//        template.setValueSerializer(jacksonSerializer);
+//        // 设置hash key 和value序列化模式
+//        template.setHashKeySerializer(stringRedisSerializer);
+//        template.setHashValueSerializer(jacksonSerializer);
+//        template.afterPropertiesSet();
+//        return template;
     }
 }
