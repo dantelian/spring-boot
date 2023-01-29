2fffb85b7617771b2e82a6bd3255a9d448516ca5
ddd <985890193@qq.com>
2023-01-29 14:03:12 +0800

    RocketMQ

---
+++ spring-boot-rocketMQ/src/test/java/com/example/springbootrocketMQ/SpringBootRocketMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootrocketMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRocketMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
