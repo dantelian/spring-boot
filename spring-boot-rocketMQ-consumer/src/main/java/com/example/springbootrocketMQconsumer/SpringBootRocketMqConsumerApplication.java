93661a49e43264fee52189c5debe0569cedfe3b0
ddd <985890193@qq.com>
2023-01-30 10:06:12 +0800

    rocketMQ-consumer

---
+++ spring-boot-rocketMQ-consumer/src/main/java/com/example/springbootrocketMQconsumer/SpringBootRocketMqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrocketMQconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRocketMqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRocketMqConsumerApplication.class, args);
+	}
+
+}
