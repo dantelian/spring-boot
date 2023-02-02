f69ddfec66d27892ff0c1ac5ca004e5cb5974755
ddd <985890193@qq.com>
2023-02-02 12:00:15 +0800

    design-pattern 单例模式

--- spring-boot-design-pattern/src/main/java/com/example/springbootdesignpattern/common/creativeMode/singletonPattern/Enumeration.java
+++ spring-boot-design-pattern/src/main/java/com/example/springbootdesignpattern/common/creativeMode/singletonPattern/Enumeration.java
@@ -1,18 +1,24 @@
 package com.example.springbootdesignpattern.common.creativeMode.singletonPattern;
 
+import com.example.springbootdesignpattern.common.holder.SpringContextHolder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public enum Enumeration {
     INSTANCE;
 
+//    private static XXService xxService = SpringContextHolder.getBean(XXService.class);
     private static int i = 0;
 
     public static int getInt() {
+        int random = (int)(Math.random()*10+1);
+        log.info("random:{}", random);
         try {
-            Thread.sleep(3000);
+            Thread.sleep(random*1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return i++;
     }
-
 
 }
--- spring-boot-design-pattern/src/main/java/com/example/springbootdesignpattern/controller/PatternController.java
+++ spring-boot-design-pattern/src/main/java/com/example/springbootdesignpattern/controller/PatternController.java
@@ -1,6 +1,7 @@
 package com.example.springbootdesignpattern.controller;
 
 import com.example.springbootdesignpattern.common.creativeMode.singletonPattern.Enumeration;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: ddd
  * @create: 2023-02-02 11:07
  **/
+@Slf4j
 @RestController
 @RequestMapping("/pattern")
 public class PatternController {
 
     @GetMapping("/test")
     public String test() {
+        long t1 = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    log.info("int:{}", Enumeration.getInt());
+                }
+            }).start();
 
-        System.out.println(Enumeration.getInt());
+        }
+        long t2 = System.currentTimeMillis();
+        log.info("time:{}", t2 - t1);
 
         return "success!";
     }
