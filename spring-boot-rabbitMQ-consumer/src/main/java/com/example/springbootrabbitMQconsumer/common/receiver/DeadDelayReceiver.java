6d14ea470767ad0b3d7e1ed0643f35687bd399f9
ddd <985890193@qq.com>
2024-03-18 16:22:47 +0800

    rabbitMQ 消费者

---
+++ spring-boot-rabbitMQ-consumer/src/main/java/com/example/springbootrabbitMQconsumer/common/receiver/DeadDelayReceiver.java
@@ -0,0 +1,19 @@
+package com.example.springbootrabbitMQconsumer.common.receiver;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+@Component
+@RabbitListener(queues = "dead_queue")
+public class DeadDelayReceiver {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " DeadDelayReceiver 消费者收到消息  : " + testMessage.toString());
+    }
+}
