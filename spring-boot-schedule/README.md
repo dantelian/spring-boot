02dddfa3cc485e277f498ad56f802a9f96256d17
ddd <985890193@qq.com>
2024-03-21 11:55:30 +0800

    spring-boot-schedule Timer

---
+++ spring-boot-schedule/README.md
@@ -0,0 +1,13 @@
+# spring-boot-schedule
+
+## Timer
+
+> 这是java自带的java.util.Timer类，这个类允许你调度一个java.util.TimerTask任务。使用这种方式可以让你的程序按照某一个频度执行，但不能在指定时间运行。一般用的较少。
+
+## 
+
+> 参考：<https://www.jb51.net/program/304716axu.htm>
+
+
+
+
---
+++ spring-boot-schedule/src/main/java/com/example/springbootschedule/timer/TimerSchedule.java
@@ -0,0 +1,24 @@
+package com.example.springbootschedule.timer;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Timer
+ * 这是java自带的java.util.Timer类，这个类允许你调度一个java.util.TimerTask任务。使用这种方式可以让你的程序按照某一个频度执行，但不能在指定时间运行。一般用的较少。
+ */
+public class TimerSchedule {
+    public static void main(String[] args) {
+        System.out.println("run:"+ new Date());
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("task  run:"+ new Date());
+            }
+        };
+        Timer timer = new Timer();
+        //安排指定的任务在指定的时间开始进行重复的固定延迟执行。这里是在执行方法1秒后每3秒执行一次
+        timer.schedule(timerTask,1000,3000);
+    }
+}
