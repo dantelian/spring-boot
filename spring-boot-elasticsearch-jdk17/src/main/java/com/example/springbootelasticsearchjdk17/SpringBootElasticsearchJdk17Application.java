5594687932296bbaf2e2a7bfdf20d2a539bf5a00
ddd <985890193@qq.com>
2024-08-02 15:02:28 +0800

    springboot elasticsearch jdk17 first commit

---
+++ spring-boot-elasticsearch-jdk17/src/main/java/com/example/springbootelasticsearchjdk17/SpringBootElasticsearchJdk17Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootelasticsearchjdk17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElasticsearchJdk17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootElasticsearchJdk17Application.class, args);
+	}
+
+}
---
+++ spring-boot-elasticsearch-jdk17/src/test/java/com/example/springbootelasticsearchjdk17/SpringBootElasticsearchJdk17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootelasticsearchjdk17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticsearchJdk17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
